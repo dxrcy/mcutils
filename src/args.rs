7c5855c171f8c7337accede4849e286b3bf0a8fd
darcy <acednes@gmail.com>
2025-04-10 10:02:25 +1000

    doc: cli interface

--- src/args.rs
+++ src/args.rs
@@ -12,15 +12,27 @@ pub struct Args {
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    /// Store a 3D block region to a file
+    ///
+    /// File will include coordinates and block data in a binary format
+    ///
+    /// Order of bounding coordinates do not matter; they will be normalized
     Save {
+        /// Name of binary file to save to
         filename: String,
+        /// First corner of 3D block region
         #[arg(value_parser = parse_coordinate)]
         origin: Coordinate,
+        /// Second corner of 3D block region
         #[arg(value_parser = parse_coordinate)]
         bound: Coordinate,
     },
 
+    /// Load a 3D block region from a file
+    ///
+    /// Always loads region at same position it was saved
     Load {
+        /// Name of binary file to load from
         filename: String,
     },
 }
